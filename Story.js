95dcce199ef9a6f48603272f46baa399b5cef33d
aaravS2 <71128251+aaravS2@users.noreply.github.com>
2021-01-25 09:54:03 +0530

    Add files via upload

---
+++ Story.js
@@ -0,0 +1,79 @@
+class Story{
+    constructor(){
+        this.heading=createElement('h1') 
+        this.instructions=createElement('h4')
+        this.story1=createElement('h4')
+        this.story2=createElement('h4')
+        this.story3=createElement('h4')
+       button=createButton('Start!') 
+       this.trajectory=[]
+    }
+    hide(){
+        this.heading.hide()
+        this.instructions.hide()
+        this.story1.hide()
+        this.story2.hide()
+        this.story3.hide()
+        button.hide()
+    }
+    story(){
+        if(gameState==='Story'){
+        this.heading.html('erf')
+        this.heading.style("color",rgb(255,0,93))
+  this.heading.position(600,100)
+this.instructions.html('Instructions:  space to jump,right arrow to turn right,left arrow to turn left')
+this.instructions.position(50,200)
+this.instructions.style("color","blue")
+this.story1.html('Story:  You are  a thief  and you broke out of the prison and the police is behind you (they have many weapons)')
+this.story1.position(50,220)
+this.story1.style("color",rgb(255,255,0))
+this.story2.html("there is an escape veichle waiting for you (you don't have much time as they will leave after some minutes)")
+this.story2.style("color",rgb(251,232,0))
+this.story2.position(50,240)
+        this.story3.html("there are many roads in front of you with a lot of traffic ")
+        this.story3.position(50,260)
+        this.story3.style("color",rgb(201,212,0))
+button.position(600,600)
+button.style('background',rgb(255,0,67))
+
+button.mousePressed(()=>{
+    gameState='play'
+    this.heading.hide()
+        this.instructions.hide()
+        this.story1.hide()
+        this.story2.hide()
+        this.story3.hide()
+        button.hide()
+})
+}
+}
+// follow(){
+    
+//     var position = [thief.x,thief.y];
+//     this.trajectory.push(position);
+    
+//     for(var i=0; i<this.trajectory.length; i++){
+//         police.x=this.trajectory[i][0]
+//       }}
+
+
+   start(){
+
+if(gameState==='Play'){
+stroke('green')
+
+    // for (var  i = 20;i<380;i=i+20) {
+    //     stroke('green')
+    //     line(i,375,i+20,375)
+        
+    //   //   road.addImage('image3',roadImg)
+      
+    
+    // console.log(bad)
+    
+};
+    drawSprites()
+}
+
+}
+
